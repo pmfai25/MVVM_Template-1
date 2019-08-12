6538d9cd3d32fed6824c0e5d33c6168c6c0217eb
Jace Malloy <jace81657@gmail.com>
2019-08-11 23:58:26 -0600

    Added DataItem stuff

---
+++ Model/DataService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MVVM_Template.Model
+{
+    public class DataService : IDataService
+    {
+        public void GetData(Action<DataItem, Exception> callback)
+        {
+            var item = new DataItem("Welcome to MVVM Light");
+            callback(item, null);
+        }
+    }
+}
